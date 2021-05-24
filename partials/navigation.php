6a8db24dad3eae2a99aedfee5034088734d3d3f4
Jesus Sanchez <jxasanchez@gmail.com>
2021-05-24 09:59:45 -0700

    add collapse navbar

--- partials/navigation.php
+++ partials/navigation.php
@@ -1,5 +1,21 @@
-<nav id="#top" class="navigation">
-    <a href="#about" class="nav-link">About</a>
-    <a href="#projects" class="nav-link">Projects</a>
-    <a href="#contact" class="nav-link">Contact</a>
-</nav> <!-- End of Navigation-->
+<nav class="navbar navbar-expand-lg">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#about">About <span class="sr-only">(About)</span></a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#projects">Projects</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#contact">Contact</a>
+            </li>
+        </ul>
+    </div>
+</nav>
